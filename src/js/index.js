8220721e8897ce319881404443c23a16cd3a7482
fazzy <kaluifeanyi00@gmail.com>
2020-06-24 01:33:08 +0100

    controller module

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,35 @@
 // Global app controller
+import Search from './modules/search';
+
+/**
+ * -search object
+ * -current recipe object
+ * -shopping list object
+ * -liked recipes
+ */
+const state = {};
+
+const controlSearch = async () =>{
+    //get the query from the view
+    const query = 'piza' //todo
+
+    if (query){
+        //new search object and add it to state
+        state.search = new Search(query);
+
+        //show loading spinner
+
+        //serach for recipe
+        await state.search.getResults();
+
+       // render results on UI
+    
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+
